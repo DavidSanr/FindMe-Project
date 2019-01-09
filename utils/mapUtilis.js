54d4e7162ec11b795cc93c6edfd68c6c37b595cc
David Sanchez <david.sanchezr@outlook.com>
2019-01-09 09:22:43 -0400

    QA

---
+++ utils/mapUtilis.js
@@ -0,0 +1,23 @@
+export function calculateDistance(pointA, pointB) {
+
+    
+    const lat1 = pointA.coordinate.latitude;
+    const lon1 = pointA.coordinate.longitude;
+  
+    const lat2 = pointB.coordinate.latitude;
+    const lon2 = pointB.coordinate.longitude;
+  
+    const R = 6371e3; 
+    const φ1 = lat1 * (Math.PI / 180);
+    const φ2 = lat2 * (Math.PI / 180);
+    const Δφ = (lat2 - lat1) * (Math.PI / 180);
+    const Δλ = (lon2 - lon1) * (Math.PI / 180);
+  
+    const a = (Math.sin(Δφ / 2) * Math.sin(Δφ / 2)) +
+              ((Math.cos(φ1) * Math.cos(φ2)) * (Math.sin(Δλ / 2) * Math.sin(Δλ / 2)));
+    
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    const distance = R * c;
+    return distance; // in meters
+  }
